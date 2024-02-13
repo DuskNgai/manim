9e5d0631e4eae3f2bdfa962df33a45dc63495042
duskngai <duskngai@gmail.com>
2024-02-14 03:09:11 +0800

    20240214.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# manim
-My manim scripts.
+# Manim
+
+My manim scripts. See [Installation](https://docs.manim.community/en/stable/installation.html) for how to install manim.
--- matmul/constants.py
+++ matmul/constants.py
@@ -28,30 +28,28 @@ __global__ void naive_gemm(
 """
 
 OPTIMIZED_GEMM_0 = """ \
-__global__ void optimized_gemm_v0(
-    float* A, float* B, float* C,
-    uint32_t M, uint32_t N, uint32_t K
-) {
-    __shared__ float shared_A[BLOCK_SIZE][BLOCK_SIZE];
-    __shared__ float shared_B[BLOCK_SIZE][BLOCK_SIZE];
+// optimized_gemm_v0
+__shared__ float shared_A[BLOCK_SIZE][BLOCK_SIZE];
+__shared__ float shared_B[BLOCK_SIZE][BLOCK_SIZE];
 
-    auto tx = threadIdx.x, ty = threadIdx.y;
-    auto row = blockIdx.y * blockDim.y + ty;
-    auto col = blockIdx.x * blockDim.x + tx;
-    if (row >= M or col >= N)
-        return;
+auto tx = threadIdx.x, ty = threadIdx.y;
+auto row = blockIdx.y * blockDim.y + ty;
+auto col = blockIdx.x * blockDim.x + tx;
 
-    float sum = 0;
-    for (uint32_t k = 0; k < K / BLOCK_SIZE; ++k) {
-        shared_A[ty][tx] = A[row * K + k * BLOCK_SIZE + tx];
-        shared_B[ty][tx] = B[(k * BLOCK_SIZE + ty) * N + col];
-        __syncthreads();
+float sum = 0.0f;
+for (uint32_t k = 0; k < div_round_up(K, BLOCK_SIZE); ++k) {
+    uint32_t col_A = k * BLOCK_SIZE + tx;
+    uint32_t row_B = k * BLOCK_SIZE + ty;
+    shared_A[ty][tx] = col_A < K ? A[row * K + col_A] : 0.0f;
+    shared_B[ty][tx] = row_B < K ? A[row_B * N + col] : 0.0f;
+    __syncthreads();
 
-        for (uint32_t i = 0; i < BLOCK_SIZE; ++i) {
-            sum += shared_A[ty][i] * shared_B[i][tx];
-        }
-        __syncthreads();
+    for (uint32_t i = 0; i < BLOCK_SIZE; ++i) {
+        sum += shared_A[ty][i] * shared_B[i][tx];
     }
+    __syncthreads();
+}
+if (row < M and col < N) {
     C[row * N + col] = sum;
 }
 """
